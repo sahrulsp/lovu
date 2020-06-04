1b23465f8a6b1f813fda6a9a16bfe5f8c9598b63
ncip48 <55126134+ncip48@users.noreply.github.com>
2020-06-05 06:10:20 +0700

    V

--- src/App.js
+++ src/App.js
@@ -68,10 +68,10 @@ class App extends Component {
       var min = new Date().getMinutes(); //Current Minutes
       var sec = new Date().getSeconds(); //Current Seconds
       if (hours >= 18) {greeting = 'Evening'}
-      elseif (hours >= 15) {greeting = 'Afternoon'}
-      elseif (hours >= 11) {greeting = 'Day'}
-      elseif (hours >= 5) {greeting = 'Morning'}
-      elseif (hours >= 0) {greeting = 'Night'}
+      else if (hours >= 15) {greeting = 'Afternoon'}
+      else if (hours >= 11) {greeting = 'Day'}
+      else if (hours >= 5) {greeting = 'Morning'}
+      else if (hours >= 0) {greeting = 'Night'}
       this.setState({
         curdate : hours + ':' + min + ':' + sec,
         greeting : greeting
