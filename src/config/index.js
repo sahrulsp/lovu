ad5d40284c46da5a40abbfaa4e5566998e79a596
sahrul sidik permana <68987737+sahrulsp@users.noreply.github.com>
2020-08-05 19:41:55 +0700

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,13 @@
 export default {
   ucapan: [
     "Whatever happens, keep breathing",
-    "Pam pam pararam ceklek jedar\nPararam ram parararam ram",
-    "Hey! I wuv chuuuu! UwU",
-    "I love you.",
+    "Komitmen berarti komunikasi. Komitmen berarti mementingkan satu sama lain di atas ego kita sendiri.\nAre you ready?",
+    "You're my sunshine in the rain when it's pouring",
+    "Loving you is like breathing How can I stop?.",
     "I may not with you everyday, but I love you everyday.",
-    "I love you.",
+    ":).",
   ],
-  panggilan: ["Difa"],
+  panggilan: ["Putih Hartini"],
   colors: [
     "#FF6900",
     "#FCB900",
